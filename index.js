cff609a454c3d8324986adff9655d4ddc3f60124
Almedina <almedinaheric30@gmail.com>
2022-03-12 17:18:23 +0100

    third commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//kada se klikne na celiju postaviti background na var(--selected-cell)
+//kada se klikne na celiju background tog reda i kolone postaviti na var(--column-row)
+//kada se klikne na celiju oznaciti sve ostale celije koje imaju  isti broj sa var(--selected-cell)
+//ako je pogresan broj color postaviti na var(--incorrect-number-color)
+//kada se rijesi jedan red/kolona/kvadrat disable-ati taj red/kolonu/kvadrat
