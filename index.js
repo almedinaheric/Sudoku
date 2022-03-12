5b63039a9ae94043bbc2cb7a7d9426adb7977a6f
Alagić Senad <senad.alagic@edu.fit.ba>
2022-03-12 17:31:27 +0100

    test push contributor

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 //kada se klikne na celiju oznaciti sve ostale celije koje imaju  isti broj sa var(--selected-cell)
 //ako je pogresan broj color postaviti na var(--incorrect-number-color)
 //kada se rijesi jedan red/kolona/kvadrat disable-ati taj red/kolonu/kvadrat
+
+function Initialize()
+{
+
+}
